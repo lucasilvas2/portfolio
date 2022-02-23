39496ee2c072c126e213b412441964f9437ea622
lucasilvas2 <lucasoliver1068@gmail.com>
2022-02-22 21:38:42 -0300

    Add scroll function with animation after click to target option

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
-$("html").ready( function() {
-    window.alert( "Attention! This page is under development..." );
-  });
+// $("html").ready( function() {
+//     window.alert( "Attention! This page is under development..." );
+// });
+
+$("#opt_home").click(function(){
+    // $("#home").scroll();
+    $('html, body').animate({
+        scrollTop: $("#home").offset().top
+    }, 100);
+});
+
+$("#opt_about").click(function(){
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 100);
+    // $("#about").scroll();
+});
+$("#opt_portifolio").click(function(){
+    $('html, body').animate({
+        scrollTop: $("#portifolio").offset().top
+    }, 100);
+});
+
+$("#opt_contact").click(function(){
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 100);
+});
+function sidebar (){
+    printConsole("click");
+}
+
+function printConsole(value){
+    console.log(value);
+}
