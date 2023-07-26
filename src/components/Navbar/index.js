ef6a39a3f461c0712cd3cd9441d26ffd4cb9c9d7
lucasilvas2 <lucasoliver1068@gmail.com>
2023-07-26 00:18:13 -0300

    feat: start frame about me

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,43 @@
+"use client";
+
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const navigation = [
+  { name: "About me", href: "#about_me", current: true },
+  { name: "Skills", href: "#skills", current: false },
+  { name: "Portfolio", href: "#", current: false },
+  { name: "Contact", href: "#", current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Navbar() {
+  return (
+    <div className="flex flex-row p-6">
+      <div className="basis-1/2">icon</div>
+      <div className="basis-1/2">
+        <div className="w-full flex justify-around space-x-4">
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className={classNames(
+                item.current
+                  ? "bg-gray-900 text-white"
+                  : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                "rounded-md px-3 py-2 text-sm font-medium"
+              )}
+              aria-current={item.current ? "page" : undefined}
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
